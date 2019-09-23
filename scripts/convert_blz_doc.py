faedaf51b48d6902d22155fdb2d2767d916ac2b3
Henryk Plötz <henryk@ploetzli.ch>
2019-09-23 19:02:05 +0200

    Change BLZ reader script: Use the text format directly

--- scripts/convert_blz_doc.py
+++ scripts/convert_blz_doc.py
@@ -1,32 +1,59 @@
-import csv
 import json
 import sys
 
+import requests
+
+
+FIELD_LENGTHS = {
+    'bank_code': 8,
+    'feature': 1,
+    'name': 58,
+    'postal_code': 5,
+    'place': 35,
+    'short_name': 27,
+    'pan': 5,
+    'bic': 11,
+    'check_digit_method': 2,
+    'record_number': 6,
+    'mod_number': 1,
+    'tbd': 1,
+    'successor_bank_code': 8,
+}
+URL = 'https://www.bundesbank.de/resource/blob/602632/b8ada8f2b30c6ede5d237be9b932a014/mL/blz-aktuell-txt-data.txt'
+
+
+def get_raw():
+    return requests.get(URL).content.decode(encoding='latin1')
+
+
+def parse(raw):
+    for line in raw.split('\n'):
+        if not line:
+            continue
+        record = {}
+        offset = 0
+        for field, length in FIELD_LENGTHS.items():
+            record[field] = line[offset:offset + length].strip()
+            offset = offset + length
+        yield record
+
+
+def process(records):
+    fieldnames = ('bank_code', 'name', 'short_name', 'bic')
+    registry = []
+    for record in records:
+        if not record['bank_code']:
+            continue
+
+        cleaned = {k: v for k, v in record.items() if k in fieldnames}
+        cleaned['primary'] = record['feature'] == '1'
+        cleaned['country_code'] = 'DE'
+
+        if cleaned['bic']:
+            registry.append(cleaned)
+    return registry
+
 
 if __name__ == '__main__':
-    with open(sys.argv[1], 'r') as fp:
-        all_fieldnames = [
-            'bank_code', 'feature', 'name', 'postal_code', 'place', 'short_name', 'pan',
-            'bic', 'check_digit_method', 'record_number', 'mod_number', 'tbd',
-            'successor_bank_code']
-        data = csv.DictReader(fp, all_fieldnames, delimiter=';')
-
-        fieldnames = ('bank_code', 'name', 'short_name', 'bic')
-        cleaned = []
-        next(data)  # Skip header line
-        for row in data:
-            if not row['bank_code'].strip():
-                continue
-            for key, value in row.items():
-                if key == 'feature':
-                    row['primary'] = value.strip() == '1'
-                if key not in fieldnames:
-                    del row[key]
-                    continue
-                row[key] = value.strip()
-            row['country_code'] = 'DE'
-            if row['bic']:
-                cleaned.append(row)
-
     with open('schwifty/bank-registry.json', 'w') as fp:
-        json.dump(cleaned, fp, indent=2)
+        json.dump(process(parse(get_raw())), fp, indent=2)
