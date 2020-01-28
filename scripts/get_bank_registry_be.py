0bea48f80f6f6e6d0e3f40eefd1a7ebf74d454c9
Kevin <18738394+KayJay89@users.noreply.github.com>
2020-01-28 23:30:22 +0100

    Corrected indentation again as per Travis

--- scripts/get_bank_registry_be.py
+++ scripts/get_bank_registry_be.py
@@ -21,8 +21,8 @@ def process(URL):
             row = sheet.row_values(i)
             column = 0
             registry_entry_template = {"bank_code": None, "bic": None,
-                                    "country_code": "BE", "name": None,
-                                        "primary": True, "short_name": None}
+                                        "country_code": "BE", "name": None,
+                                    "primary": True, "short_name": None}
             skip_row = False
             for cell in row:
                 if column == 1:
