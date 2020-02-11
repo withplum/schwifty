1d6d477965123bc20a26df4b8d2679bd313dacfa
Kevin <18738394+KayJay89@users.noreply.github.com>
2020-02-11 15:10:53 +0100

    Removed try/catch, removed if/and and replaced with shorthand if third cell empty

--- scripts/get_bank_registry_be.py
+++ scripts/get_bank_registry_be.py
@@ -9,35 +9,23 @@ URL = "https://www.nbb.be/doc/be/be/protocol/r_fulllist_of_codes_current.xlsx"
 def process():
     registry = []
     skip_names = ["NAV", "VRIJ", "NAP", "NYA", "VRIJ - LIBRE", "-"]
-    book = xlrd.open_workbook(file_contents=requests.get(URL).content)
 
-    try:
-        sheet = book.sheet_by_index(0)
-        for row in list(sheet.get_rows())[2:]:
-            bank_code, bic, name, second_name = row[:4]
-            if bic.value.upper() in skip_names:
-                continue
-            if name.value != "":
-                registry.append({
-                    "country_code": "BE",
-                    "primary": True,
-                    "bic": bic.value.upper().replace(" ", ""),
-                    "bank_code": bank_code.value,
-                    "name": name.value,
-                    "short_name": name.value,
-                })
-            else:
-                registry.append({
-                    "country_code": "BE",
-                    "primary": True,
-                    "bic": bic.value.upper().replace(" ", ""),
-                    "bank_code": bank_code.value,
-                    "name": second_name.value,
-                    "short_name": second_name.value,
-                })
-        return registry
-    except IndexError:
-        raise
+    book = xlrd.open_workbook(file_contents=requests.get(URL).content)
+    sheet = book.sheet_by_index(0)
+
+    for row in list(sheet.get_rows())[2:]:
+        bank_code, bic, name, second_name = row[:4]
+        if bic.value.upper() in skip_names:
+            continue
+        registry.append({
+            "country_code": "BE",
+            "primary": True,
+            "bic": bic.value.upper().replace(" ", ""),
+            "bank_code": bank_code.value,
+            "name": name.value or second_name.value,
+            "short_name": name.value or second_name.value,
+        })
+    return registry
 
 
 if __name__ == '__main__':
