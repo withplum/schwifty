65cdf4ba2620b88519f785a95c7970461bdb0238
Martin Domke <mail@martindomke.net>
2020-05-08 16:49:47 +0200

    Update LV banks and script

--- scripts/get_bank_registry_lv.py
+++ scripts/get_bank_registry_lv.py
@@ -1,30 +1,26 @@
 #!/usr/bin/env python
 import json
-import xlrd
+
 import requests
+import xlrd
 
 URL = "https://www.bank.lv/images/stories/pielikumi/makssist/bic_saraksts_22.01.2020_eng.xls"
 
 
 def process():
-    registry = []
-
     book = xlrd.open_workbook(file_contents=requests.get(URL).content)
     sheet = book.sheet_by_index(0)
-
-    for row in list(sheet.get_rows())[2:]:
-        name, bank_code, bic = row[1:]
-        registry.append(
-            {
-                "country_code": "LV",
-                "primary": True,
-                "bic": bic.value.upper(),
-                "bank_code": bank_code.value[4:8],
-                "name": name.value,
-                "short_name": name.value,
-            }
-        )
-    return registry
+    return [
+        {
+            "country_code": "LV",
+            "primary": True,
+            "bic": bic.value.upper(),
+            "bank_code": bic.value[:4],
+            "name": name.value,
+            "short_name": name.value,
+        }
+        for _id, name, _iban_structure, bic in list(sheet.get_rows())[2:]
+    ]
 
 
 if __name__ == "__main__":
