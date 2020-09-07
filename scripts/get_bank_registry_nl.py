eed1bf456514a5d5d30a72327d15880ee36b7f94
insensitiveclod <contact@sociallife.org>
2020-09-07 20:43:19 +0200

    Added scraper script for NL-bic list (#35)
    * Added NL BICs to bank_registry
    
    * NL BIC support
    removed unused variables from scraper
    
    * NL BIC support
    Removed unneeded data-mongering

---
+++ scripts/get_bank_registry_nl.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import json
+
+import requests
+import xlrd
+
+
+URL = "https://www.betaalvereniging.nl/wp-content/uploads/BIC-lijst-NL.xlsx"
+
+
+def process():
+    registry = []
+
+    book = xlrd.open_workbook(file_contents=requests.get(URL).content)
+    sheet = book.sheet_by_index(0)
+
+    for row in list(sheet.get_rows())[2:]:
+        bic, bank_code, name = row[:3]
+        registry.append(
+            {
+                "country_code": "NL",
+                "primary": True,
+                "bic": bic.value.upper(),
+                "bank_code": bank_code.value,
+                "name": name.value,
+                "short_name": name.value,
+            }
+        )
+    return registry
+
+
+if __name__ == "__main__":
+    with open("schwifty/bank_registry/generated_nl.json", "w") as fp:
+        json.dump(process(), fp, indent=2)
