950e840c8ea9a1b03a018e13afc8db6a4a77e097
Martin Domke <mail@martindomke.net>
2020-12-02 14:59:02 +0100

    Update PL bank registry and fix data source errors

--- scripts/get_bank_registry_pl.py
+++ scripts/get_bank_registry_pl.py
@@ -6,6 +6,12 @@ import requests
 URL = "https://ewib.nbp.pl/plewibnra?dokNazwa=plewibnra.txt"
 
 
+bic_remapping = {
+    "PLUPLPRXXX": "POLUPLPRXXX",
+    "POLUPLPRXX": "POLUPLPRXXX",
+}
+
+
 def process():
     registry = []
     with requests.get(URL, stream=True) as fp:
@@ -13,11 +19,12 @@ def process():
             cells = [cell.strip() for cell in row.decode("CP852").split("\t")]
             if len(cells) != 32:
                 continue
+            bic = cells[19].upper()
             registry.append(
                 {
                     "country_code": "PL",
                     "primary": True,
-                    "bic": cells[19].upper(),
+                    "bic": bic_remapping.get(bic, bic),
                     "bank_code": cells[4],
                     "name": cells[1],
                     "short_name": cells[1],
