1b31dc6d16b69300054a381f750e931176fd9d2d
Thorin Schiffer <thorin@schiffer.pro>
2020-12-15 14:33:29 +0100

    Added custom exceptions

---
+++ schwifty/exceptions.py
@@ -0,0 +1,38 @@
+class UnknownCountryCode(ValueError):
+    pass
+
+
+class InvalidBankCodeForCountry(ValueError):
+    pass
+
+
+class InvalidLength(ValueError):
+    pass
+
+
+class InvalidStructure(ValueError):
+    pass
+
+
+class InvalidCountryCode(ValueError):
+    pass
+
+
+class TooLongBankCode(ValueError):
+    pass
+
+
+class TooLongBranchCode(ValueError):
+    pass
+
+
+class TooLongAccountCode(ValueError):
+    pass
+
+
+class InvalidIBANCharacters(ValueError):
+    pass
+
+
+class InvalidChecksumDigits(ValueError):
+    pass
