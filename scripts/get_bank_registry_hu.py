cf874b5777663cfb0b2784d0122a84bb41e71a85
Kryštof Řeháček <krystofee@gmail.com>
2022-06-17 11:01:20 +0200

    Added Hungarian BIC codes with generator (#96)

---
+++ scripts/get_bank_registry_hu.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import json
+from tempfile import NamedTemporaryFile
+
+import openpyxl
+import requests
+
+
+BANK_CODES_URL = "https://www.mnb.hu/letoltes/sht.xlsx"
+
+
+def process():
+    yielded_bic_codes = set()
+
+    with NamedTemporaryFile(suffix="data.xlsx") as temp_file:
+        response = requests.get(BANK_CODES_URL)
+        temp_file.write(response.content)
+        workbook = openpyxl.load_workbook(temp_file.name)
+        sheet = workbook.worksheets[0]
+
+        for row_i, line in enumerate(sheet):
+            if row_i > 1:
+                bic_code = str(line[1].value).upper()
+                if bic_code not in yielded_bic_codes:
+                    yielded_bic_codes.add(bic_code)
+                    bank_name = str(line[2].value)
+
+                    yield {
+                        "country_code": "HU",
+                        "primary": True,
+                        "bic": bic_code,
+                        "bank_code": bic_code[:4],
+                        "name": bank_name,
+                        "short_name": bank_name,
+                    }
+
+
+if __name__ == "__main__":
+    with open("schwifty/bank_registry/generated_hu.json", "w") as fp:
+        json.dump(list(process()), fp, indent=2)
--- scripts/update-all.sh
+++ scripts/update-all.sh
@@ -4,7 +4,7 @@ set -o errexit
 main() {
   local -r root=$(dirname "${BASH_SOURCE[0]}")
 
-  for country in "at" "be" "cz" "de" "es" "fi" "hr" "lv" "nl" "pl" "si" "sk"; do
+  for country in "at" "be" "cz" "de" "es" "fi" "hr" "hu" "lv" "nl" "pl" "si" "sk"; do
     echo "---> Updating bank registry for: ${country}"
     python "${root}/get_bank_registry_${country}.py"
   done
