9779ed475231ed173f96adb3a8fd202ebab3c33d
Martin Domke <mail@martindomke.net>
2022-06-29 23:24:50 +0200

    chore: Fix linter errors in LT registry script

--- scripts/get_bank_registry_lt.py
+++ scripts/get_bank_registry_lt.py
@@ -4,34 +4,32 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
+
 URL = "https://www.lb.lt/zinynai/branches.aspx"
+
 
 def process():
     registry = []
     xml_content = requests.get(URL).text
     soup = BeautifulSoup(xml_content, "xml")
-    participants = soup.find_all("participant") # participants are primary bank names in LB list
+    participants = soup.find_all("participant")  # participants are primary bank names in LB list
 
     for participant in participants:
         branches = participant.find_all("branch")
-        for index, branch in enumerate(branches):   
+        for index, branch in enumerate(branches):
             try:
-                if index > 0:
-                    primary = False
-                else:
-                    primary = True
                 entry = {
                     "country_code": "LT",
                     "bic": participant.find("BIC").get_text(),
                     "bank_code": branch.find("FICODE").get_text(),
                     "name": branch.find("NAME").get_text(),
                     "short_name": participant.find("NAME").get_text(),
-                    "primary": primary
+                    "primary": index == 0,
                 }
                 registry.append(entry)
-            except AttributeError as e:
+            except AttributeError:
                 pass
-    
+
     return registry
 
 
